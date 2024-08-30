141344fa773cfda0fbe489083b8be0fdd1b8ba84
yifeili <tonysup86@gmail.com>
2024-08-30 16:55:05 -0400

    created test C++ files

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
 //
 // Created by Yifei Li on 8/30/24.
 //
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    printf("Hello World!");
+    return 0;
+}
